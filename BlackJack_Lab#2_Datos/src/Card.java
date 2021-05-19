2f63e6bf5d714842f6c33ec47c9b625b20d60d7d
KemJiga <kemerj@uninorte.edu.co>
2021-05-19 00:52:30 -0500

    i guess it is completed
    maybe change some "not-list" code lines

---
+++ BlackJack_Lab#2_Datos/src/Card.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kemer
+ */
+public class Card {
+
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String toString() {
+        return this.value.toString() + "-" + this.suit.toString();
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+}
---
+++ BlackJack_Lab#2_Datos/src/Value.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kemer
+ */
+public enum Value {
+    DOS, TRES, CUATRO, CINCO, SEIS, SIETE, OCHO, NUEVE, DIES, JACK, QUEEN, KING, AZ
+}
